d9e38ac718a38e13f9d7947a668867e0e33d39bd
Yuya Nishimura <yuya@YuyaNishimuranoMacBook-Pro.local>
2019-04-17 09:39:34 +0900

    検索がヒットしなかった時の処理、ルートページ設定

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,14 +7,16 @@ class HomeController < ApplicationController
   def search
     keyword = set_params[:search]
     url = URI.encode "https://api.themoviedb.org/3/search/movie?api_key=b80d7b2dcc71906bb510ce9d4b43b879&query=#{keyword}&language=ja-JP"
-    @movies = JSON.load(open(url))["results"]
+    json = JSON.load(open(url))
+
+    @movies = json["results"]
     render 'show'
   end
-  
+
   private
 
   def set_params
     params.require(:home).permit(:search)
   end
-  
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  root 'home#index'
   get 'home/index'
+  post '/' => 'home#search'
   post 'home/index' => 'home#search'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
