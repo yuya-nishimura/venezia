7ce7522e610d39169a433b425f887b9cd301bd5a
Yuya Nishimura <yuya@YuyaNishimuranoMacBook-Pro.local>
2019-04-18 18:14:44 +0900

    Listコントローラーcreateアクション、作成用フォーム追加

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -10,11 +10,26 @@ class ListsController < ApplicationController
   end
 
   def create
+    @list = current_user.lists.build(list_params)
+    if @list.save
+      flash[:success] = "リストが作成されました"
+      redirect_to root_url
+    else
+      error_messages = @list.errors.full_messages.join(', ')
+      flash[:danger] = error_messages
+      redirect_to root_url
+    end
   end
 
   def update
   end
 
   def destroy
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:name, :description)
   end
 end
