8f80d6f86839122a8d257c0cbeb6e1c6d8b8c813
Yuya Nishimura <yuya@YuyaNishimuranoMacBook-Pro.local>
2019-04-18 11:06:35 +0900

    認証失敗時のルートリダイレクト追加

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,10 +6,14 @@ class SessionsController < Devise::OmniauthCallbacksController
     if @user.persisted?
       sign_in_and_redirect @user
 
-    # 新規ユーザーならセッションに値を保存してルートにリダイレクト
+    # ユーザーが保存出来ていなければルートにリダイレクト
     else
-      session["devise.user_attributes"] = @user.attributes
       redirect_to root_url
     end
+  end
+
+  # 認証失敗時にルートへリダイレクトする
+  def after_omniauth_failure_path_for(scope)
+    root_path
   end
 end
