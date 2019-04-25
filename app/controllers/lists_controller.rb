3774b689a5de4a91319eaaf6ee0965962e98cf91
Yuya Nishimura <yuya@YuyaNishimuranoMacBook-Pro.local>
2019-04-25 12:24:36 +0900

    並び替え機能完成

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -2,8 +2,17 @@ class ListsController < ApplicationController
 before_action :set_list, except: [:create]
 
   def show
+    # 並び替え用のクエリがあるか確認
+    sort_key = params[:sort_by] ? params[:sort_by].to_sym : :created_at
+    # クエリはこちらで用意したもの以外は無効とする
+    sort_values = { created_at:   [:created_at, '登録順'],
+                    title:        [:title, 'タイトル順'],
+                    release_date: [:release_date, '公開日順'] }
+    raise unless sort_values.has_key?(sort_key)
+
     # ページネーション用にMovieオブジェクト群を取り出す
-    @movies = @list.movies.page(params[:page]).per(15)
+    @movies = @list.movies.order(sort_values[sort_key][0]).page(params[:page]).per(15)
+    @sort_name = sort_values[sort_key][1]
   end
 
   def create
--- app/models/list.rb
+++ app/models/list.rb
@@ -7,6 +7,9 @@ class List < ApplicationRecord
   belongs_to :user
   has_many :movies, dependent: :destroy
 
+  # リストは新しいものが上に追加されていく
+  default_scope { order(created_at: :desc) }
+
   # リストのサムネイルを表示する
   def thumbnail
     movies.first ? movies.first.resized_poster(45) : 'noimage.png'
