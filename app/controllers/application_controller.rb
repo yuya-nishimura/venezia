724393586b28659bc991047758e31e95752524ae
Yuya Nishimura <yuya@YuyaNishimuranoMacBook-Pro.local>
2019-04-26 12:16:16 +0900

    認証系追加、seachアクションの改修(ヘッダーのフォームを変更、リザルト画面からの再検索実装)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  before_action :set_user
+  before_action :set_user, :authenticate_user!
 
   private
 
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,6 @@
 class HomeController < ApplicationController
+  skip_before_action :authenticate_user!, only: :index
+  before_action :set_params, only: :search
   require 'open-uri'
 
   def index
@@ -7,13 +9,15 @@ class HomeController < ApplicationController
 
   def search
     api_key = ENV["TMDB_API_KEY"]
-    keyword = set_params[:search]
+    @keyword = params[:search]
+    @page = params[:page] ? params[:page].to_i : 1
 
-    if !keyword.blank?
-      url = URI.encode "https://api.themoviedb.org/3/search/movie?api_key=#{api_key}&query=#{keyword}&page=1&language=ja-JP&region=JP"
+    if !@keyword.blank?
+      url = URI.encode "https://api.themoviedb.org/3/search/movie?api_key=#{api_key}&query=#{@keyword}&page=#{@page}&language=ja-JP&region=JP"
       json = JSON.load(open(url))
       @results = json["results"]
       @total_results = json["total_results"]
+      @total_pages = json["total_pages"]
 
       # セレクトボックスのリストにはページネーションをかけない
       @lists = @user.lists
@@ -31,7 +35,7 @@ class HomeController < ApplicationController
   # end
 
   def set_params
-    params.require(:home).permit(:search)
+    params.permit(:search, :page)
   end
 
 end
